a38ff8f590caa38e2b1aad868159039262bcf109
Ashutosh Kumar <68853189+typedashutosh@users.noreply.github.com>
2021-06-25 21:00:50 +0530

    Deploy Next.js to gh-pages

---
+++ _next/static/Tcr2HW1NkLzRzLDLm4Dd1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f402bd7ddd722afc3822.css","static/chunks/pages/index-30724fffb6eba29e9824.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
