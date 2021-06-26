e6ce62b50e8773bf41235f2670a55df4b5e5292f
Ashutosh Kumar <68853189+typedashutosh@users.noreply.github.com>
2021-06-26 10:11:21 +0530

    finally gh-pages works

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const debug = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-	assetPrefix: !debug ? '/typedashutosh.github.io/' : ''
+	// assetPrefix: !debug ? '/typedashutosh.github.io/' : ''
 }
